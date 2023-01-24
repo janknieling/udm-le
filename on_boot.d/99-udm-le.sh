fdb5a93c52abb67267b838899756a4c77725c475
Jan Knieling <jan.knieling@ida-analytics.de>
2023-01-24 19:37:59 +0100

    Added data dir detection based on UniFi Dream Machine model/firmware

--- on_boot.d/99-udm-le.sh
+++ on_boot.d/99-udm-le.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
+# Set data dir
+DATA_DIR="/mnt/data"
+case "$(ubnt-device-info firmware || true)" in
+    1*)
+      DATA_DIR="/mnt/data"
+      ;;
+    2*)
+      DATA_DIR="/data"
+      ;;
+    3*)
+      DATA_DIR="/data"
+      ;;
+    *)
+      echo "ERROR: No persistent storage found." 1>&2
+      exit 1
+      ;;
+  esac
+
+
 # Load Environmnt Variables
-. /mnt/data/udm-le/udm-le.env
+. $DATA_DIR/udm-le/udm-le.env
 
 if [ ! -f /etc/cron.d/udm-le ]; then
 	# Sleep for 5 minutes to avoid restarting
